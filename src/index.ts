2e1caaea033bda91a953324a4221fe73cc5049bf
jieeeun2 <jieun2.dev@gmail.com>
2024-01-04 11:46:41 +0900

    chore: helmet 기본 설정 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
+import helmet from 'helmet'
 
 const app = express()
 
 app.use(express.json()) //JSON 형태의 요청 body 파싱
 app.use(express.urlencoded({ extended: true })) //x-www-form-urlencoded 형태의 요청 body를 파싱
+
+app.use(helmet())
