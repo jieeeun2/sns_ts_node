d921e6bb4862c11760d06e91e08b8daebb999eea
jieeeun2 <jieun2.dev@gmail.com>
2024-01-04 23:46:22 +0900

    feat: User모델 생성

---
+++ src/model/User.ts
@@ -0,0 +1,31 @@
+import { Schema, Types, model } from 'mongoose'
+
+interface UserType {
+  name: string
+  email: string
+  password: string
+  avatarPath?: string
+  friends?: Types.ObjectId[]
+  location?: string
+  occupation?: string
+  viewedProfile: number
+  impressions: number
+}
+
+const UserSchema = new Schema<UserType>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  avatarPath: { type: String },
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  location: { type: String },
+  occupation: { type: String },
+  viewedProfile: { type: Number },
+  impressions: { type: Number },
+}, { 
+  timestamps: true 
+})
+
+const User = model<UserType>('User', UserSchema)
+
+export default User
