6ab4b73a87c924d88dbc5078ded85e186a974f57
jieeeun2 <jieun2.dev@gmail.com>
2024-01-04 11:54:39 +0900

    chore: morgan 사용한 요청/응답 로그 설정

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import helmet from 'helmet'
+import morgan from 'morgan'
 
 const app = express()
 
@@ -7,3 +8,5 @@ app.use(express.json()) //JSON 형태의 요청 body 파싱
 app.use(express.urlencoded({ extended: true })) //x-www-form-urlencoded 형태의 요청 body를 파싱
 
 app.use(helmet())
+
+app.use(morgan('dev'))
