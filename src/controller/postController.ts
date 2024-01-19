8f1a265d6b15dbcc7adcd06ada0f309393514e85
jieeeun2 <jieun2.dev@gmail.com>
2024-01-19 12:23:23 +0900

    feat: 게시물등록 api구현

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -1,4 +1,13 @@
 import { Request, Response } from 'express'
+import Post from 'model/Post'
+import { Types } from 'mongoose'
+
+interface User {
+  _id: Types.ObjectId
+  name: string
+  profileImagePath: string
+  location: string
+}
 
 export const getAllPostList = async (req: Request, res: Response) => {
   
@@ -9,7 +18,55 @@ export const getPostList = async (req: Request, res: Response) => {
 }
 
 export const createPost = async (req: Request, res: Response) => {
-  
+  try {
+    const { userId, content, imageFiles } = req.body
+    
+    const newPost = new Post({ 
+      userId: new Types.ObjectId(userId),
+      content, 
+      //imagePaths //TODO: 이미지업로드 미들웨어에서 imageFiles 가공해서 imagePaths넘겨줘야함
+    })
+    await newPost.save()
+
+    const populatedPost = await Post
+      .findById(newPost._id)
+      .populate<{ userId: User }>({
+        path: 'userId',
+        select: '_id name profileImagePath location'
+      })
+
+    if(!populatedPost) return
+    
+    const { 
+      _id: postId, 
+      userId: {
+        _id: postUserId,
+        ...restUserData
+      },
+      likes,
+      comments,
+      ...restData
+    } = populatedPost.toObject()
+
+    const numberOfLikes = Object.keys(likes).length
+    const numberOfComments = comments.length
+
+    const responseData = {
+      id: postId.toString(),
+      user: {
+        userId: postUserId.toString(),
+        ...restUserData
+      },
+      numberOfLikes,
+      numberOfComments,
+      ...restData
+    }
+
+    res.status(201).json({ message: '게시물이 등록되었습니다.', data: responseData })
+  } catch(err: any) {
+    console.log({ error: err.message })
+    res.status(404).send({ message: '게시물 등록에 실패했습니다. 다시 시도해 주세요.' })
+  }
 }
 
 export const modifyPost = async (req: Request, res: Response) => {
