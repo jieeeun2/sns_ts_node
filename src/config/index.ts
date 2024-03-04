b20a2ec2b5d8c4faa54d84a26262b076e3d5359e
jieeeun2 <jieun2.dev@gmail.com>
2024-03-04 12:34:27 +0900

    feat: 파일업로드 관련 aws config임을 명확하게 하기위한 변수명 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,13 +9,13 @@ export const mongoDBConfig = {
   port: process.env.PORT,
 }
 
-export const awsS3Config = {
-  bucketName: process.env.S3_BUCKET_NAME,
-  accessKey: process.env.S3_ACCESS_KEY,
-  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-}
-
 export const jwtConfig = {
   secretKey: process.env.JWT_SECRET_KEY,
   secretRefreshKey: process.env.JWT_SECRET_REFRESH_KEY,
+}
+
+export const fileUploadAWSConfig = {
+  bucketName: process.env.FILE_UPLOAD_AWS_S3_BUCKET_NAME,
+  accessKey: process.env.FILE_UPLOAD_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.FILE_UPLOAD_AWS_SECRET_ACCESS_KEY,
 }
